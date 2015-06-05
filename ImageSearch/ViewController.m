e5b72a24582c266aa80ce592ef210173192a44b7
Admim <admin@Admims-MBP.fios-router.home>
2015-06-05 17:48:17 -0400

    set up basic collection view skeleton

--- ImageSearch/ViewController.m
+++ ImageSearch/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@property (nonatomic, strong) UICollectionView *imageCollectionView;
 @end
 
 @implementation ViewController
@@ -17,7 +17,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Image Search";
+    [self setupImageCollectionVC];
 }
+
+#pragma mark - setup imageCollectionVC
+- (void)setupImageCollectionVC
+{
+    // set a flow layout for the collection view
+    UICollectionViewFlowLayout *aFlowLayout = [[UICollectionViewFlowLayout alloc] init];
+    [aFlowLayout setItemSize:CGSizeMake(100, 100)];
+    [aFlowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    [aFlowLayout setMinimumInteritemSpacing:5];
+    [aFlowLayout setSectionInset:UIEdgeInsetsMake(15, 5, 15, 5)];
+    
+    // initialize collection view
+    self.imageCollectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:aFlowLayout];
+    self.imageCollectionView.delegate = self;
+    self.imageCollectionView.dataSource = self;
+    [self.imageCollectionView setBackgroundColor:[UIColor blackColor]];
+    [self.view addSubview:self.imageCollectionView];
+}
+
+#pragma mark - UICollectionView Data Source
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"imageCell";
+    [self.imageCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:cellIdentifier];
+    UICollectionViewCell *cell = [self.imageCollectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
